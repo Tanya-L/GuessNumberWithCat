8baef7f9c381af2fffd0b987f2670f070391dd02
User <User@DESK-FD7TP52.lexicon.edu.local>
2017-12-28 11:23:18 +0100

    First commit

---
+++ src/java/controlen/GuessNumberBean.java
@@ -0,0 +1,69 @@
+package controlen;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.Random;
+
+@Named(value = "guessNumberBean")
+@SessionScoped
+public class GuessNumberBean implements Serializable {
+
+    private final int RANDOMNUMBER;
+    
+    private int max = 10;
+    private int min = 1;
+    private int userguess;
+    
+    private String response;        
+            
+    public GuessNumberBean() {
+        Random random = new Random();
+        RANDOMNUMBER = random.nextInt(max)+1;
+    }
+    
+    public String getRespons(){
+    if (userguess==RANDOMNUMBER) {
+        return "I can not belived. You guessed right!";
+    } else if (userguess<RANDOMNUMBER){ 
+        return "Sorry, my dear. "
+                + "You guessed to low number. "
+                + "Do you want to try one more time?";
+    } else { 
+        return "Sorry, my dear. You guessed to high number. Do you want to try one more time?";
+    }
+}
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getUserguess() {
+        return userguess;
+    }
+
+    public void setUserguess(int userguess) {
+        this.userguess = userguess;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    } 
+}
+
